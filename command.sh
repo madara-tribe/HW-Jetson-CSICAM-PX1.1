a40bee86dab7b23ba8a39bbfce5bb4df171e2582
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2022-11-26 02:20:49 +0900

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+#https://mirai-tec.hatenablog.com/entry/2021/08/09/175835
 wget https://www.waveshare.com/w/upload/e/eb/Camera_overrides.tar.gz
 tar zxvf Camera_overrides.tar.gz
 sudo cp camera_overrides.isp /var/nvidia/nvcam/settings/
