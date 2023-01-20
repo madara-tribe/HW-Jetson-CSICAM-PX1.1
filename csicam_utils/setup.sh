39dd3c0fd62165c75e31b0fea27fed2273fd22e6
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2023-01-20 23:06:30 +0900

    Update setup.sh

--- csicam_utils/setup.sh
+++ csicam_utils/setup.sh
@@ -2,5 +2,6 @@
 #https://mirai-tec.hatenablog.com/entry/2021/08/09/175835
 wget https://www.waveshare.com/w/upload/e/eb/Camera_overrides.tar.gz
 tar zxvf Camera_overrides.tar.gz
-sudo cp camera_overrides.isp /var/nvidia/nvcam/settings/
-sudo chmod 664 /var/nvidia/nvcam/settings/camera_overrides.ispsudo chown root:root /var/nvidia/nvcam/settings/camera_overrides.isp
+cp camera_overrides.isp /var/nvidia/nvcam/settings/
+chmod 664 /var/nvidia/nvcam/settings/camera_overrides.isp
+chown root:root /var/nvidia/nvcam/settings/camera_overrides.isp
