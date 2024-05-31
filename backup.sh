68c0a1efb4dfc59f28301317c9667af92b689121
madara-tribe <tsukuba.univer@gmail.com>
2024-05-31 22:19:32 +0900

    cleanup

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-find . -name '.DS_Store' -type f -ls -delete
-mkdir -p bp bp/jetson_utils bp/qtWidgets bp/csicam_utils
-cp *.py *.sh bp/
-cp qtWidgets/*.py bp/qtWidgets/
-cp jetson_utils/*.py bp/jetson_utils/
-cp jetson_utils/*.cpp bp/jetson_utils/
-cp csicam_utils/* bp/csicam_utils/
